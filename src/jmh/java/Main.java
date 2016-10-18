4e6171421bb7f108a9b348f01d5b45aeab9238ee
Sebastian Graf <sgraf1337@gmail.com>
2016-10-18 18:28:52 +0200

    Got rid of jmh

--- src/jmh/java/Main.java
+++
@@ -1,9 +0,0 @@
-import org.openjdk.jmh.runner.RunnerException;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException, RunnerException {
-        org.openjdk.jmh.Main.main(args);
-    }
-}
