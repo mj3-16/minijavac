e0e99a2b86cc94e9f94343e0ff34cd6a55b13305
Johannes Bechberger <me@mostlynerdless.de>
2016-10-19 10:26:14 +0200

    Add "call run in other dir" test case
    And fix issues that prevent this test case to succeed.

--- src/test/java/minijava/CliTest.java
+++ src/test/java/minijava/CliTest.java
@@ -1,9 +1,11 @@
 package minijava;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.nio.file.Files;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,21 +15,45 @@ import static org.junit.Assert.assertEquals;
 public class CliTest {
 
 	/**
+	 * Temporary file that should just contain the string "test"
+	 */
+	private static File tmpFile;
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		tmpFile = File.createTempFile("tmp", "tmp");
+		tmpFile.deleteOnExit();
+		FileWriter writer = new FileWriter(tmpFile);
+		writer.write("t");
+		writer.close();
+	}
+
+	/**
 	 * Tests the `--echo` option of the cli by calling the `run` script
-     */
+	 */
 	@Test
 	public void echo() throws Exception {
 		Process process = Runtime.getRuntime().exec(new String[]{"./run", "--echo", "non_existing_file"});
 		process.waitFor();
 		assertEquals("Error code for invalid --echo call", 1, process.exitValue());
-		File tmpFile = File.createTempFile("tmp", "tmp");
-		FileWriter writer = new FileWriter(tmpFile);
-		writer.write("t");
-		writer.close();
+
+
 		process = Runtime.getRuntime().exec(new String[]{"./run", "--echo", tmpFile.getAbsolutePath()});
 		process.waitFor();
 		assertEquals(0, process.exitValue());
 		assertEquals('t', process.getInputStream().read());
-		tmpFile.delete();
+	}
+
+	@Test
+	public void echoFromATemporaryDirectory() throws Exception {
+		File tmpDirectory = Files.createTempDirectory("test").toFile();
+		tmpDirectory.deleteOnExit();
+		ProcessBuilder builder = new ProcessBuilder(new File("run").getAbsolutePath(), "--echo",
+				tmpFile.getAbsolutePath());
+		builder.directory(tmpDirectory);
+		Process process = builder.start();
+		process.waitFor();
+		assertEquals("Error code", 0, process.exitValue());
+		assertEquals('t', process.getInputStream().read());
 	}
 }
