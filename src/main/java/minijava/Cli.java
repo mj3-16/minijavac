76d35fab4dba25d0e2ede0c7102e9ae095027a60
Johannes Bechberger <me@mostlynerdless.de>
2016-10-19 11:48:25 +0200

    Print the usage info for invalid arguments

--- src/main/java/minijava/Cli.java
+++ src/main/java/minijava/Cli.java
@@ -2,6 +2,7 @@ package minijava;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -20,15 +21,34 @@ class Cli {
     private final PrintStream out;
     private final PrintStream err;
     private final JCommander jCommander;
+    /**
+     * Did the command argument parsing fail?
+     */
+    private final boolean invalidArguments;
 
 
     Cli(OutputStream out, OutputStream err, String... args) {
         this.out = new PrintStream(out);
         this.err = new PrintStream(err);
-        this.jCommander = new JCommander(this, args);
+        JCommander commander;
+        try {
+            commander = new JCommander(this, args);
+        } catch (ParameterException ex){
+            commander = null;
+            this.err.println(ex.getMessage());
+            this.err.println();
+        }
+        this.jCommander = new JCommander(this);
+        this.invalidArguments = commander == null;
     }
 
     int run() {
+        if (invalidArguments){
+            StringBuilder sb = new StringBuilder();
+            jCommander.usage(sb);
+            err.print(sb.toString());
+            return 1;
+        }
         if(help) {
             StringBuilder sb = new StringBuilder();
             jCommander.usage(sb);
