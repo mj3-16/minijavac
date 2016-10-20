eea4f7ecd509cbadcc15f9b2ac81e5c98aa8ae33
Simon Herter <simon.herter@student.kit.edu>
2016-10-20 16:14:25 +0200

    Apply google-java-format (only whitespace changes)

--- src/main/java/minijava/Cli.java
+++ src/main/java/minijava/Cli.java
@@ -4,7 +4,6 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 import com.google.common.io.ByteStreams;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,53 +12,52 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 class Cli {
-    @Parameter(names = "--echo", description = "print given file on stdout")
-    private String echoPath;
+  @Parameter(names = "--echo", description = "print given file on stdout")
+  private String echoPath;
 
-    @Parameter(names = "--help", help = true, description = "print this usage information")
-    private boolean help;
+  @Parameter(names = "--help", help = true, description = "print this usage information")
+  private boolean help;
 
+  private final PrintStream out;
+  private final PrintStream err;
+  private final JCommander jCommander;
 
-    private final PrintStream out;
-    private final PrintStream err;
-    private final JCommander jCommander;
+  private Cli(OutputStream out, OutputStream err, String... args) {
+    this.out = new PrintStream(out);
+    this.err = new PrintStream(err);
+    this.jCommander = new JCommander(this, args);
+  }
 
-    private Cli(OutputStream out, OutputStream err, String... args) {
-        this.out = new PrintStream(out);
-        this.err = new PrintStream(err);
-        this.jCommander = new JCommander(this, args);
+  public static Cli create(OutputStream out, OutputStream err, String... args)
+      throws InvalidCommandLineArguments {
+    try {
+      return new Cli(out, err, args);
+    } catch (ParameterException ex) {
+      throw new InvalidCommandLineArguments(ex.getMessage());
     }
+  }
 
-    public static Cli create(OutputStream out, OutputStream err, String... args) throws InvalidCommandLineArguments {
-        try {
-            return new Cli(out, err, args);
-        } catch (ParameterException ex){
-            throw new InvalidCommandLineArguments(ex.getMessage());
-        }
+  int run() {
+    if (help) {
+      out.print(getUsageInfo());
+      return 0;
     }
-
-    int run() {
-        if(help) {
-            out.print(getUsageInfo());
-            return 0;
-        }
-        if (echoPath != null) {
-            try {
-                InputStream in = Files.newInputStream(Paths.get(echoPath));
-                ByteStreams.copy(in, out);
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace(err);
-                return 1;
-            }
-        }
-        return 0;
+    if (echoPath != null) {
+      try {
+        InputStream in = Files.newInputStream(Paths.get(echoPath));
+        ByteStreams.copy(in, out);
+        in.close();
+      } catch (IOException e) {
+        e.printStackTrace(err);
+        return 1;
+      }
     }
+    return 0;
+  }
 
-    public static String getUsageInfo(){
-        StringBuilder sb = new StringBuilder();
-        new Cli(System.out, System.err).jCommander.usage(sb);
-        return sb.toString();
-    }
-
+  public static String getUsageInfo() {
+    StringBuilder sb = new StringBuilder();
+    new Cli(System.out, System.err).jCommander.usage(sb);
+    return sb.toString();
+  }
 }
--- src/main/java/minijava/InvalidCommandLineArguments.java
+++ src/main/java/minijava/InvalidCommandLineArguments.java
@@ -1,11 +1,9 @@
 package minijava;
 
-/**
- * Thrown for invalid command line arguments.
- */
+/** Thrown for invalid command line arguments. */
 public class InvalidCommandLineArguments extends Exception {
 
-    public InvalidCommandLineArguments(String message) {
-        super(message);
-    }
+  public InvalidCommandLineArguments(String message) {
+    super(message);
+  }
 }
