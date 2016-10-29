ef4208792aff4f0352afe46054a996feaa55fd17
Simon Herter <simon.herter@student.kit.edu>
2016-10-30 01:29:13 +0200

    Refactor CLI and corresponding tests to be more straight forward
    I tried to clarify and simplify things. The ugly parts (that deal with
    the JCommander library) were moved into a separate class.
    
    Blackbox tests that use the ./run script were removed. We have the
    python based test suites for that. Some other tests for command line
    options validation were added.
    
    I'm not sure why the outputLexerTokens method was so complicated before,
    I hope I didn't miss something?!

--- src/main/java/minijava/Main.java
+++ src/main/java/minijava/Main.java
@@ -1,15 +1,12 @@
 package minijava;
 
+import java.nio.file.FileSystems;
+
 public class Main {
   public static void main(String[] args) {
     //new LexerRepl(SimpleLexer::new).run(); //← to test the lexer
-    try {
-      System.exit(Cli.create(System.out, System.err, args).run());
-    } catch (InvalidCommandLineArguments invEx) {
-      System.err.println(invEx.getMessage());
-      System.err.println();
-      System.err.println(Cli.getUsageInfo());
-      System.exit(1);
-    }
+    Cli cli = new Cli(System.out, System.err, FileSystems.getDefault());
+    int status = cli.run(args);
+    System.exit(status);
   }
 }
