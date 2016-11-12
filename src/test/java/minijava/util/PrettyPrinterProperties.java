31aad87188bee63cf3b7b1d47b2e4efe1f1cacda
Sebastian Graf <sgraf1337@gmail.com>
2016-11-12 22:25:59 +0100

    Commented out debug printfs

--- src/test/java/minijava/util/PrettyPrinterProperties.java
+++ src/test/java/minijava/util/PrettyPrinterProperties.java
@@ -4,8 +4,6 @@ import com.pholser.junit.quickcheck.From;
 import com.pholser.junit.quickcheck.Property;
 import com.pholser.junit.quickcheck.generator.Size;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
-import difflib.DiffUtils;
-import difflib.Patch;
 import java.util.Arrays;
 import java.util.List;
 import minijava.ast.Program;
@@ -27,28 +25,28 @@ public class PrettyPrinterProperties {
     String expected = p.program.acceptVisitor(PRETTY_PRINTER).toString();
 
     // Debug printfs:
-    System.out.println("expected:");
-    System.out.println(expected);
+    //    System.out.println("expected:");
+    //    System.out.println(expected);
 
     Program<String> parsed = new Parser(new Lexer(expected)).parse();
 
     String actual = parsed.acceptVisitor(PRETTY_PRINTER).toString();
 
     // Debug printfs:
-    System.out.println("actual:");
-    System.out.println(actual);
-
-    Patch<String> patch = DiffUtils.diff(listOfLines(expected), listOfLines(actual));
-    System.out.println("diff:");
-    patch
-        .getDeltas()
-        .forEach(
-            d -> {
-              System.out.println(String.format("[%04d]:", d.getOriginal().getPosition()));
-              System.out.println(d.getOriginal().getLines());
-              System.out.println("-----------------------------------------");
-              System.out.println(d.getRevised().getLines());
-            });
+    //    System.out.println("actual:");
+    //    System.out.println(actual);
+    //
+    //    Patch<String> patch = DiffUtils.diff(listOfLines(expected), listOfLines(actual));
+    //    System.out.println("diff:");
+    //    patch
+    //        .getDeltas()
+    //        .forEach(
+    //            d -> {
+    //              System.out.println(String.format("[%04d]:", d.getOriginal().getPosition()));
+    //              System.out.println(d.getOriginal().getLines());
+    //              System.out.println("-----------------------------------------");
+    //              System.out.println(d.getRevised().getLines());
+    //            });
 
     Assert.assertEquals(expected, actual);
   }
