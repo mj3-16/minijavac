f95caedfb1a4f92da06fafc52ae5f129848666d4
Sebastian Graf <sgraf1337@gmail.com>
2016-11-19 14:37:16 +0100

    Fixed that lexer NUL bug

--- src/main/java/minijava/lexer/Lexer.java
+++ src/main/java/minijava/lexer/Lexer.java
@@ -71,8 +71,9 @@ public class Lexer implements Iterator<Token> {
   private int ch = -2;
   private int line = 1;
   private int column = 0;
-  private Token eof;
   private SourcePosition tokenBegin;
+  private Token eof;
+  private boolean inlineNUL = false;
 
   public Lexer(InputStream input) {
     this.input = new BufferedInputStream(input);
@@ -106,6 +107,7 @@ public class Lexer implements Iterator<Token> {
       } else {
         column++;
       }
+      inlineNUL = ch == 0 && input.available() > 0;
     } catch (IOException e) {
       throw new MJError(e);
     }
@@ -124,8 +126,10 @@ public class Lexer implements Iterator<Token> {
       switch (ch) {
         case -1:
         case 0:
-          eof = createToken(EOF);
-          return eof;
+          if (inlineNUL) {
+            throw new LexerError(new SourcePosition(line, column), "Invalid NUL byte");
+          }
+          return (eof = createToken(EOF));
         case '+':
           nextChar();
           return scanPlus();
