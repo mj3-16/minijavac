a99ca9f9deef3b5996b32ced1dbeb7a26c7ad40c
Johannes Bechberger <me@mostlynerdless.de>
2016-11-24 14:02:02 +0100

    Improve local variable counting visitor

--- src/main/java/minijava/firm/NumberOfLocalVariablesVisitor.java
+++ src/main/java/minijava/firm/NumberOfLocalVariablesVisitor.java
@@ -1,15 +1,23 @@
 package minijava.firm;
 
 import minijava.ast.Block;
+import minijava.ast.BlockStatement;
 import minijava.ast.Expression;
 import minijava.ast.Statement;
 
+import javax.swing.text.StyledEditorKit;
+import java.util.stream.Collectors;
+
 /** Created by parttimenerd on 23.11.16. */
 public class NumberOfLocalVariablesVisitor
-    implements Block.Visitor<Integer>, Expression.Visitor<Integer> {
+    implements Block.Visitor<Integer>, Expression.Visitor<Integer>, BlockStatement.Visitor<Integer> {
   @Override
   public Integer visitBlock(Block that) {
-    return that.acceptVisitor(this);
+    int c = 0;
+    for (BlockStatement statement : that.statements) {
+      c += statement.acceptVisitor(this); // TODO: improve algorithm
+    }
+    return c;
   }
 
   @Override
@@ -23,71 +31,57 @@ public class NumberOfLocalVariablesVisitor
     if (that.else_.isPresent()) {
       elseNum = that.else_.get().acceptVisitor(this);
     }
-    return Math.max(
-        Math.max(that.condition.acceptVisitor(this), that.then.acceptVisitor(this)), elseNum);
+    return Math.max(that.then.acceptVisitor(this), elseNum);
   }
 
   @Override
   public Integer visitExpressionStatement(Statement.ExpressionStatement that) {
-    return that.expression.acceptVisitor(this);
+    return 0;
   }
 
   @Override
   public Integer visitWhile(Statement.While that) {
-    return Math.max(that.condition.acceptVisitor(this), that.body.acceptVisitor(this));
+    return that.body.acceptVisitor(this);
   }
 
   @Override
   public Integer visitReturn(Statement.Return that) {
-    if (that.expression.isPresent()) {
-      return that.expression.get().acceptVisitor(this);
-    }
     return 0;
   }
 
   @Override
   public Integer visitBinaryOperator(Expression.BinaryOperator that) {
-    int app = 0;
-    if (that.op == Expression.BinOp.ASSIGN){
-      app += 1;
-    }
-    return Math.max(that.left.acceptVisitor(this), that.right.acceptVisitor(this)) + app;
+    return 0;
   }
 
   @Override
   public Integer visitUnaryOperator(Expression.UnaryOperator that) {
-    return that.expression.acceptVisitor(this);
+    return 0;
   }
 
   @Override
   public Integer visitMethodCall(Expression.MethodCall that) {
-    return Math.max(
-        1,
-        that.arguments
-            .stream()
-            .map(e -> e.acceptVisitor(this))
-            .max(Integer::compare)
-            .orElseGet(() -> 0));
+    return 0;
   }
 
   @Override
   public Integer visitFieldAccess(Expression.FieldAccess that) {
-    return Math.max(1, that.self.acceptVisitor(this)) + 1;
+    return 0;
   }
 
   @Override
   public Integer visitArrayAccess(Expression.ArrayAccess that) {
-    return Math.max(1, Math.max(that.index.acceptVisitor(this), that.array.acceptVisitor(this))) + 1;
+    return 0;
   }
 
   @Override
   public Integer visitNewObject(Expression.NewObject that) {
-    return 1;
+    return 0;
   }
 
   @Override
   public Integer visitNewArray(Expression.NewArray that) {
-    return Math.max(1, that.size.acceptVisitor(this));
+    return 0;
   }
 
   @Override
@@ -108,5 +102,10 @@ public class NumberOfLocalVariablesVisitor
   @Override
   public Integer visitReferenceTypeLiteral(Expression.ReferenceTypeLiteral that) {
     return 0;
+  }
+
+  @Override
+  public Integer visitVariable(BlockStatement.Variable that) {
+    return 1;
   }
 }
