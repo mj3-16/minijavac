50dad4d765fe6a4a83732b0ee413989c39c7c561
Simon Herter <simon.herter@student.kit.edu>
2016-11-24 11:39:37 +0100

    Workaround for Firm.init()

---
+++ src/test/java/minijava/firm/InitFirmTest.java
@@ -0,0 +1,14 @@
+package minijava.firm;
+
+import firm.Mode;
+import org.junit.Test;
+
+public class InitFirmTest {
+
+  @Test
+  public void initWithWrapperTwiceDoesNotThrow() throws Exception {
+    InitFirm.init();
+    InitFirm.init();
+    Mode.getIs(); // make sure Firm is initialized (this call would throw otherwise)
+  }
+}
