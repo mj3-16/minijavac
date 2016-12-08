a9e12f46f3cf5980b50bb2c66c5963a108c37088
Simon Herter <simon.herter@student.kit.edu>
2016-12-08 14:04:42 +0100

    Remove manually inserted Bad nodes first
    I'm not sure if binding_irgopt.remove_unreachable_code() recursively
    detects unreachable code (i.e. code that became uncreachable, because
    something else became unreachable). However, it for sure doesn't
    pick up manually inserted Bad nodes. Therefore, we first remove the
    manually inserted ones and then call remove_uncreachable_code (and
    hope that it does work recursively).

--- src/main/java/minijava/ir/optimize/UnreachableCodeRemover.java
+++ src/main/java/minijava/ir/optimize/UnreachableCodeRemover.java
@@ -7,7 +7,10 @@ public class UnreachableCodeRemover implements Optimizer {
 
   @Override
   public void optimize(Graph graph) {
-    binding_irgopt.remove_unreachable_code(graph.ptr);
+    // first remove Bad nodes we inserted manually (the remove_unreachable_code function doesn't pick them up)
+    binding_irgopt.remove_bads(graph.ptr);
+    binding_irgopt.remove_unreachable_code(
+        graph.ptr); // find and replace unreachable code with Bad nodes
     binding_irgopt.remove_bads(graph.ptr);
   }
 }
