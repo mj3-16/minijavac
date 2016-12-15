25d74c3e830651442ef0d203edcf65f913cb46b4
Simon Herter <simon.herter@student.kit.edu>
2016-12-15 14:11:30 +0100

    Simplify constant folding for phi nodes
    Immediately mark Lode nodes as bad.

--- src/main/java/minijava/ir/optimize/ConstantFolder.java
+++ src/main/java/minijava/ir/optimize/ConstantFolder.java
@@ -1,6 +1,5 @@
 package minijava.ir.optimize;
 
-import com.google.common.collect.Iterables;
 import firm.BackEdges;
 import firm.Graph;
 import firm.TargetValue;
@@ -15,19 +14,14 @@ import java.util.stream.StreamSupport;
 public class ConstantFolder extends BaseOptimizer {
 
   private Map<Node, TargetValue> latticeMap;
-
   private TargetValue previousValue;
   private TargetValue newValue;
-  private NodeVisitor phiStrategy;
 
   @Override
   public boolean optimize(Graph graph) {
     this.latticeMap = new HashMap<>();
     this.graph = graph;
     BackEdges.enable(graph);
-    phiStrategy = new SupremumStrategy();
-    fixedPointIteration();
-    phiStrategy = new EqualAndConstantStrategy();
     fixedPointIteration();
     boolean changed = replaceConstants();
     BackEdges.disable(graph);
@@ -131,6 +125,13 @@ public class ConstantFolder extends BaseOptimizer {
   }
 
   @Override
+  public void visit(Load node) {
+    newValue = TargetValue.getBad();
+    previousValue = latticeMap.put(node, newValue);
+    hasChanged = detectChange();
+  }
+
+  @Override
   public void visit(Minus node) {
     visitUnaryOperation(TargetValue::neg, node, node.getOp());
   }
@@ -177,7 +178,25 @@ public class ConstantFolder extends BaseOptimizer {
 
   @Override
   public void visit(Phi node) {
-    phiStrategy.visit(node);
+    newValue =
+        StreamSupport.stream(node.getPreds().spliterator(), false)
+            .map(n -> latticeMap.getOrDefault(n, TargetValue.getUnknown()))
+            .reduce(TargetValue.getUnknown(), (a, b) -> supremum(a, b));
+    previousValue = latticeMap.put(node, newValue);
+    hasChanged = detectChange();
+  }
+
+  private static TargetValue supremum(TargetValue a, TargetValue b) {
+    if (a.equals(b)) {
+      return a; // or b
+    }
+    if (a.equals(TargetValue.getUnknown())) {
+      return b;
+    }
+    if (b.equals(TargetValue.getUnknown())) {
+      return a;
+    }
+    return TargetValue.getBad();
   }
 
   @Override
@@ -203,56 +222,5 @@ public class ConstantFolder extends BaseOptimizer {
   @Override
   public void visit(Sub node) {
     visitBinaryOperation((lhs, rhs) -> lhs.sub(rhs), node, node.getLeft(), node.getRight());
-  }
-
-  private class SupremumStrategy extends DefaultNodeVisitor {
-    @Override
-    public void visit(Phi node) {
-      newValue =
-          StreamSupport.stream(node.getPreds().spliterator(), false)
-              .map(n -> latticeMap.getOrDefault(n, TargetValue.getUnknown()))
-              .reduce(TargetValue.getUnknown(), (a, b) -> supremum(a, b));
-      previousValue = latticeMap.put(node, newValue);
-      hasChanged = detectChange();
-    }
-  }
-
-  private static TargetValue supremum(TargetValue a, TargetValue b) {
-    if (a.equals(b)) {
-      return a; // or b
-    }
-    if (a.equals(TargetValue.getUnknown())) {
-      return b;
-    }
-    if (b.equals(TargetValue.getUnknown())) {
-      return a;
-    }
-    return TargetValue.getBad();
-  }
-
-  private class EqualAndConstantStrategy extends DefaultNodeVisitor {
-    @Override
-    public void visit(Phi node) {
-      Node first = Iterables.getFirst(node.getPreds(), null);
-      if (first == null) {
-        // Phi node has no inputs
-        newValue = TargetValue.getBad();
-      } else {
-        // Phi node has at least 'first' as input
-        TargetValue firstValue = latticeMap.getOrDefault(first, TargetValue.getUnknown());
-        boolean allInputsAreEqual =
-            StreamSupport.stream(node.getPreds().spliterator(), false)
-                .skip(1)
-                .map(n -> latticeMap.getOrDefault(n, TargetValue.getUnknown()))
-                .allMatch(value -> value.equals(firstValue));
-        if (allInputsAreEqual && firstValue.isConstant()) {
-          newValue = firstValue;
-        } else {
-          newValue = TargetValue.getBad();
-        }
-      }
-      previousValue = latticeMap.put(node, newValue);
-      hasChanged = detectChange();
-    }
   }
 }
