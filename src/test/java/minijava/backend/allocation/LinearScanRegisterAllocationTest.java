3951367c9d3980647a275b6e5c9516df2fc58fcc
Sebastian Graf <sgraf1337@gmail.com>
2017-02-17 23:00:10 +0100

    At least assert that registers were split correctly

--- src/test/java/minijava/backend/allocation/LinearScanRegisterAllocationTest.java
+++ src/test/java/minijava/backend/allocation/LinearScanRegisterAllocationTest.java
@@ -6,12 +6,13 @@ import static minijava.backend.registers.AMD64Register.DI;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.iterableWithSize;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 import com.google.common.collect.Sets;
 import java.util.List;
 import java.util.Set;
 import minijava.backend.ExampleProgram;
-import minijava.backend.VirtualRegisterSupply;
+import minijava.backend.lifetime.BlockPosition;
 import minijava.backend.lifetime.LifetimeAnalysis;
 import minijava.backend.lifetime.LifetimeAnalysisResult;
 import minijava.backend.lifetime.LifetimeInterval;
@@ -21,7 +22,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class LinearScanRegisterAllocationTest {
-  private final VirtualRegisterSupply supply = new VirtualRegisterSupply();
   private static Set<AMD64Register> ONE_REG = Sets.newHashSet(DI);
   private static Set<AMD64Register> TWO_REGS = Sets.newHashSet(A, DI);
   private static Set<AMD64Register> THREE_REGS = Sets.newHashSet(A, B, DI);
@@ -30,11 +30,11 @@ public class LinearScanRegisterAllocationTest {
   public void loopCountingToFiveThreeRegs_doesntSpill() {
     ExampleProgram example = ExampleProgram.loopCountingToFive();
     LifetimeAnalysisResult lifetimes = LifetimeAnalysis.analyse(example.program);
-    AllocationResult allocationResult =
-        LinearScanRegisterAllocator.allocateRegisters(lifetimes, THREE_REGS);
+    AllocationResult result = LinearScanRegisterAllocator.allocateRegisters(lifetimes, THREE_REGS);
 
-    Assert.assertTrue(
-        "Should not have spilled any register", allocationResult.spillSlots.isEmpty());
+    assertLifetimesMatch(lifetimes, result);
+
+    Assert.assertTrue("Should not have spilled any register", result.spillSlots.isEmpty());
   }
 
   @Test
@@ -42,6 +42,8 @@ public class LinearScanRegisterAllocationTest {
     ExampleProgram example = ExampleProgram.loopCountingToFive();
     LifetimeAnalysisResult lifetimes = LifetimeAnalysis.analyse(example.program);
     AllocationResult result = LinearScanRegisterAllocator.allocateRegisters(lifetimes, TWO_REGS);
+
+    assertLifetimesMatch(lifetimes, result);
 
     // r0 is the constant 5, which is the argument to the Cmp
     VirtualRegister r0 = example.registers.get(0);
@@ -75,15 +77,39 @@ public class LinearScanRegisterAllocationTest {
 
     result.printDebugInfo();
 
-    // r0 is the constant 5, which is the argument to the Cmp
-    VirtualRegister r0 = example.registers.get(0);
-    assertIsSplitOnceAtItsUse(result, r0);
+    assertLifetimesMatch(lifetimes, result);
 
-    // r2 is the constant 1, which is the argument to the Add instruction in the loop
-    VirtualRegister r2 = example.registers.get(2);
-    assertIsSplitOnceAtItsUse(result, r2);
+    // Not really sure what to verify. There are many ways this has a correct outcome, most of which
+    // are not simply stated.
+  }
 
-    // All other registers shouldn't have been split.
-    Assert.assertTrue("Shouldn't have split other registers", result.spillSlots.size() == 2);
+  private void assertLifetimesMatch(LifetimeAnalysisResult lifetimes, AllocationResult result) {
+    for (List<LifetimeInterval> splits : result.splitLifetimes.values()) {
+      for (int i = 0; i < splits.size() - 1; ++i) {
+        LifetimeInterval prev = splits.get(i);
+        LifetimeInterval next = splits.get(i + 1);
+        Assert.assertThat(
+            "Splits of the same register may not overlap",
+            prev.ranges.firstIntersectionWith(next.ranges),
+            nullValue());
+        BlockPosition endPrev = prev.ranges.to();
+        BlockPosition startNext = next.ranges.from();
+        Assert.assertTrue(
+            "Splits of the same register may not have holes",
+            !endPrev.block.equals(startNext.block) || endPrev.pos + 1 == startNext.pos);
+      }
+
+      LifetimeInterval first = splits.get(0);
+      LifetimeInterval last = splits.get(splits.size() - 1);
+      LifetimeInterval original = lifetimes.virtualIntervals.get(first.register);
+      Assert.assertEquals(
+          "Splits should start with the lifetime interval of the original register",
+          first.ranges.from(),
+          original.ranges.from());
+      Assert.assertEquals(
+          "Splits should end with the lifetime interval of the original register",
+          last.ranges.to(),
+          original.ranges.to());
+    }
   }
 }
